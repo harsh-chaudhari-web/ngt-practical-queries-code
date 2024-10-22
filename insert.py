2325b73da11b8ec317a801a0daa5bfeab23d50b1
Harsh Chaudhari <111692385+harsh-chaudhari-web@users.noreply.github.com>
2024-10-23 02:48:54 +0530

    insert co

---
+++ insert.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost:27017')
+
+db = client.employeedata
+
+def insert():
+
+try:
+
+eid = input('Enter Employee id :')
+
+ename = input('Enter Name :')
+
+eage = input('Enter age :')
+
+ecountry =input('Enter Country :')
+
+db.employees.insert_one(
+
+{ "eid": eid, "ename":ename, "eage":eage, "ecountry":ecountry })
+
+print("\nInserted data successfully\n")
+
+except Exception:
+
+print(str(e))
+
+insert()
